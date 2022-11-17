851246e0bfcba62193c43ef36ca645d63f8b292f
Konstantin Borimechkov <konstantinmb@yahoo.com>
2022-11-17 08:10:33 +0200

    Laying the base of the project. Configuring DB settings and adding the proper maven dependencies.

---
+++ mypfinance-backend/src/main/java/com/project/mypfinance/MypfinanceApplication.java
@@ -0,0 +1,13 @@
+package com.project.mypfinance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MypfinanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MypfinanceApplication.class, args);
+	}
+
+}
