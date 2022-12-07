7f9244321df04986ff8b02d0daa025aa6339b83b
Konstantin Borimechkov <konstantinmb@yahoo.com>
2022-12-07 09:07:05 +0200

    Revert forntend changes, removed on accident by me.

--- mypfinance-frontend/src/store/index.js
+++ mypfinance-frontend/src/store/index.js
@@ -1,9 +1,29 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import { alert } from '@/store/modules/alert'
+import { loader } from '@/store/modules/loader'
+import { account } from '@/store/modules/account'
+import createPersistedState from 'vuex-persistedstate'
+import SecureLS from 'secure-ls'
+const ls = new SecureLS({ isCompression: false })
 
 Vue.use(Vuex)
 
 const store = new Vuex.Store({
+  modules: {
+    alert,
+    loader,
+    account
+  },
+  plugins: [
+    createPersistedState({
+      storage: {
+        getItem: key => ls.get(key),
+        setItem: (key, value) => ls.set(key, value),
+        removeItem: key => ls.remove(key)
+      }
+    })
+  ]
 
 })
 
