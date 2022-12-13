39560adccff567255277664eb98ae8c1a7a581fe
Blagovest Georgiev <74932934+YukiYuuna@users.noreply.github.com>
2022-12-13 14:32:33 +0200

    Handle income categories fetching and add store (#22)
    * Add income categories querymutations and store to handle usage
    
    * Add modules to centralized store

---
+++ mypfinance-frontend/src/services/income-categories-services.js
@@ -0,0 +1,57 @@
+import axios from 'axios'
+import authHeader from '@/services/auth-header'
+
+const API_URL = 'http://localhost:8080/'
+const headers = {
+  withCredentials: false,
+  headers: {
+    Authorization: authHeader(),
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*'
+  }
+}
+
+class IncomeCategoriesService {
+  getAllIncomeCategories () {
+    return axios.get(API_URL + 'api/income/categories', headers
+    ).then(response => {
+      return response.data
+    })
+  }
+
+  createIncomeCategory (incomeCategory) {
+    const requestCategory = {
+      categoryName: incomeCategory.incomeCategory.categoryName,
+      color: incomeCategory.incomeCategory.color
+    }
+
+    return axios.post(API_URL + 'api/add/income/category', requestCategory, headers
+    ).then(response => {
+      return response.data
+    })
+  }
+
+  modifyIncomeCategory (modifiedCategory) {
+    const requestCategory = {
+      categoryName: modifiedCategory.incomeCategory.categoryName,
+      color: modifiedCategory.incomeCategory.color
+    }
+    const incomeCategoryId = modifiedCategory.incomeCategory.incomeCategoryId
+
+    return axios.put(API_URL + 'api/modify/income/category/' + incomeCategoryId, requestCategory, headers
+    ).then(response => {
+      return response.data
+    })
+  }
+
+  deleteIncomeCategory (incomeCategoryId) {
+    const id = incomeCategoryId.incomeCategoryId
+
+    return axios.delete(API_URL + 'api/delete/income/category/' + id, headers
+    ).then(response => {
+      return response.data
+    })
+  }
+}
+
+export default new IncomeCategoriesService()
